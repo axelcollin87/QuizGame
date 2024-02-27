4707b446b1bfe7e1da6f14d485f8756d914968a8
Axel Collin <shoulder_87@hotmail.com>
2024-02-28 00:12:19 +0100

    added functionality for answer validation, all except checkbox variant working

--- axel.js
+++ axel.js
@@ -5,6 +5,8 @@ const submitButton = document.getElementById("submitBtn");
 const questionContainer = document.getElementById("question-container");
 const personalForm = document.getElementById("personal-form");
 
+let questionData = [];
+
 personalForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -22,30 +24,67 @@ personalForm.addEventListener("submit", (event) => {
     loadQuestions();
 });
 
+submitButton.addEventListener("click", () => {
+    let score = 0;
+
+    questionData.forEach((question) => {
+        switch (question.type) {
+            case "text":
+                const textAnswer = document.querySelector(
+                    `input[name='${question.id}']`
+                );
+                if (textAnswer.value === question.answer) {
+                    console.log(`${question.answer} is correct!`);
+                } else {
+                    console.log(
+                        `Wrong answer, correct answer is: ${question.answer}`
+                    );
+                }
+                break;
+            case "radio":
+                const radioSelection = document.querySelector(
+                    `input[name='${question.id}']:checked`
+                );
+                if (radioSelection.value === question.answer) {
+                    console.log(`${question.answer} is correct!`);
+                } else {
+                    console.log(
+                        `Wrong answer, correct answer is: ${question.answer}`
+                    );
+                }
+                break;
+
+            // figure out how to do checkbox case, probably use querySelectorAll.
+        }
+    });
+});
+
 function loadQuestions() {
     fetch(
         "https://raw.githubusercontent.com/axelcollin87/quizquestions/master/quiz_data.json"
     )
         .then((response) => response.json())
         .then((data) => {
-            data.questions.forEach((que) => {
-                switch (que.type) {
+            questionData = data.questions;
+
+            data.questions.forEach((q) => {
+                switch (q.type) {
                     case "text":
-                        questionContainer.innerHTML += `<p class="questionP">${que.question}</p>`;
-                        questionContainer.innerHTML += `<input type='text' placeholder='Type answer here'/>`;
+                        questionContainer.innerHTML += `<p class="questionP">${q.question}</p>`;
+                        questionContainer.innerHTML += `<input type='text' name='${q.id}' placeholder='Type answer here'/>`;
                         break;
                     case "radio":
-                        questionContainer.innerHTML += `<p class="questionP">${que.question}</p>`;
-                        que.options.forEach((option) => {
-                            questionContainer.innerHTML += `<input type='radio' name='question' value='${option}'/>`;
+                        questionContainer.innerHTML += `<p class="questionP">${q.question}</p>`;
+                        q.options.forEach((option) => {
+                            questionContainer.innerHTML += `<input type='radio' name='${q.id}' value='${option}'/>`;
                             questionContainer.innerHTML += `<label for='${option}'>${option}</label>`;
                             questionContainer.innerHTML += `<br />`;
                         });
                         break;
                     case "checkbox":
-                        questionContainer.innerHTML += `<p class="questionP">${que.question} (Multi choice)</p>`;
-                        que.options.forEach((option) => {
-                            questionContainer.innerHTML += `<input type='checkbox' name='question' value='${option}'/>`;
+                        questionContainer.innerHTML += `<p class="questionP">${q.question} (Multi choice)</p>`;
+                        q.options.forEach((option) => {
+                            questionContainer.innerHTML += `<input type='checkbox' name='${q.id}' value='${option}'/>`;
                             questionContainer.innerHTML += `<label for='${option}'>${option}</label>`;
                             questionContainer.innerHTML += `<br />`;
                         });
