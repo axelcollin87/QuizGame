a9b6c24afcfc19ce5236536e7d1d4362c3d64131
Axel Collin <shoulder_87@hotmail.com>
2024-02-27 13:23:13 +0100

    added functionality to load html into question div, different layouts switching on question type from json object, added submit btn

--- axel.js
+++ axel.js
@@ -1,6 +1,8 @@
 const fName = document.getElementById("firstName");
 const lName = document.getElementById("lastName");
 const email = document.getElementById("email");
+const submitButton = document.getElementById("submitBtn");
+const questionContainer = document.getElementById("question-container");
 const personalForm = document.getElementById("personal-form");
 
 personalForm.addEventListener("submit", (event) => {
@@ -27,7 +29,30 @@ function loadQuestions() {
         .then((response) => response.json())
         .then((data) => {
             data.questions.forEach((que) => {
-                console.log(que.question);
+                switch (que.type) {
+                    case "text":
+                        questionContainer.innerHTML += `<p class="questionP">${que.question}</p>`;
+                        questionContainer.innerHTML += `<input type='text' placeholder='Type answer here'/>`;
+                        break;
+                    case "radio":
+                        questionContainer.innerHTML += `<p class="questionP">${que.question}</p>`;
+                        que.options.forEach((option) => {
+                            questionContainer.innerHTML += `<input type='radio' name='question' value='${option}'/>`;
+                            questionContainer.innerHTML += `<label for='${option}'>${option}</label>`;
+                            questionContainer.innerHTML += `<br />`;
+                        });
+                        break;
+                    case "checkbox":
+                        questionContainer.innerHTML += `<p class="questionP">${que.question} (Multi choice)</p>`;
+                        que.options.forEach((option) => {
+                            questionContainer.innerHTML += `<input type='checkbox' name='question' value='${option}'/>`;
+                            questionContainer.innerHTML += `<label for='${option}'>${option}</label>`;
+                            questionContainer.innerHTML += `<br />`;
+                        });
+                        break;
+                }
             });
         });
+    questionContainer.style.display = "block";
+    submitButton.style.display = "block";
 }
